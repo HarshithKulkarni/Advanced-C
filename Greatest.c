6a0cac7bdca259c0eb41db8c30ddf8030ab3d96c
HarshithKulkarni <harshithkulkarni@gmail.com>
2018-02-23 08:30:55 +0530

    Update Greatest.c

--- Greatest.c
+++ Greatest.c
@@ -1,23 +1,24 @@
+
 #include<stdio.h>
-#include<conio.h>
+
 int compare(int a,int b, int c);
 
-int compare(int a,int b, int c)  //function declaration
+int compare(int a,int b, int c)
 {
     if(a>b && a>c)
     {
-        printf("The greatest number is %d\n",a);
-            return a;
+
+            return 1;
     }
     else if(b>a && b>c)
     {
-        printf("The greatest number is %d\n",b);
-          return b;
+
+          return 2;
     }
     else if(c>a && c>b)
     {
-        printf("The greatest number is %d\n",c);
-          return c;
+
+          return 3;
     }
 
 }
@@ -27,7 +28,22 @@ int main()
     int a,b,c;
     printf("Enter the numbers to be compared \n");
     scanf("%d%d%d",&a,&b,&c);
-    compare(a,b,c);  //function call
+    int comp=compare(a,b,c);
+    switch (comp)
+  {
+case 1:
+     printf("The greatest number is %d\n",a);
+     break;
+case 2:
+     printf("The greatest number is %d\n",b);
+     break;
+case 3:
+       printf("The greatest number is %d\n",c);
+       break;
+  }
+
+
+
     return 0;
 
 }
