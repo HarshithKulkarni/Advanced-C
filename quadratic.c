a69ce81453f1610792319ee492cba2533b9eca9c
HarshithKulkarni <harshithkulkarni@gmail.com>
2018-04-09 18:12:33 +0530

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -1,43 +1,45 @@
 
+
 #include<stdio.h>
 #include<math.h>
 struct complex
 {
-float real,imag;
-};
-float det(int a,int b,int c);
+   double  real;
+   double  imag;
+}s1,s2;
+float computeroots(int a,int b,int c,int d);
 float det(int a,int b,int c)
 {
-    float det;
-    det=b*b-4*a*c;
-    printf("The value is %f",det);
-     return det;
+    float d;
+    d=b*b-4*a*c;
+    printf("The value is %f",d);
+    computeroots(a,b,c,d);
 }
-float computeroots(int a,int b,int c,struct complex *r1,struct complex *r2)
+float computeroots(int a,int b,int c,int d)
 {
-     struct complex r1,r2;
-     if(det>0)
+
+     float r1,r2,real;
+     if(d>0)
    {
-     r1=-b+(sqrt(det))/(2*a);
-     r2=-b-(sqrt(detr))/(2*a);
+     r1=-b+(sqrt(d))/(2*a);
+     r2=-b-(sqrt(d))/(2*a);
      printf("The roots are real and distinct %f %f \n",r1,r2);
      return 0;
    }
-    else if(det==0)
+    else if(d==0)
    {
     r1=r1=-b/(2*a);
-    printf("The roots are real and equal %f",r1);
+    printf("The roots are real and equal %f\n",r1);
     return 0;
    }
 
-     else if(det<0)
+     else if(d<0)
    {
-    r1.real=r2.real=-b/(2*a);
-    r1.imag=r2.imag=(sqrt(det*-1))/(2*a);
-    printf("The roots are imaginary %.2f+i%.2f and %.2f-i%.2f",real,imag,real,imag);
+    s1.real=s2.real=-b/(2*a);
+    s1.imag=s2.imag=(sqrt(d*-1))/(2*a);
+    printf("The roots are imaginary %.2f+i%.2f and %.2f-i%.2f\n",s1.real,s1.imag,s2.real,s2.imag);
    return 0;
    }
-}
 }
 int main()
 {
@@ -45,6 +47,8 @@ int main()
     printf("Enter the coeffecients\n");
     scanf("%d%d%d",&a,&b,&c);
     det(a,b,c);
-    computeroots(a,b,c,&r1,&r2);
 return 0;
 }
+
+
+
